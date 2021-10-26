4136b04a81e927d7c5cd526b2c24333adc142b9e
Samy Basso-Bert <90609046+Samybassobert73@users.noreply.github.com>
2021-10-26 20:49:52 +0200

    Delete backend directory

--- backend/server.js
+++
@@ -1,9 +0,0 @@
-const http = require('http');
-// require app
-const app = require('./app');
-
-
-app.set('port', process.env.PORT || 3000);
-const server = http.createServer(app);
-
-server.listen(process.env.PORT || 3000);
